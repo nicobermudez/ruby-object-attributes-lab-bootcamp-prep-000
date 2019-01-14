a262c1cc4746cc055da9dd03b1ee8dd04837c15d
Nicolas Bermudez <bermnicolas@gmail.com>
2019-01-14 22:41:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,5 @@
 class Dog 
+  
   def initialize(name)
     @name = name
   end
@@ -8,6 +9,19 @@ class Dog
   end
   
   def name=(new_name)
-    @name = name
+    @name = new_name
+  end
+
+  def initialize(breed)
+    @breed = breed
+  end
+  
+  def breed 
+    @breed 
+  end
+  
+  def breed=(new_breed)
+    @breed = new_breed 
+  end
     
 end
